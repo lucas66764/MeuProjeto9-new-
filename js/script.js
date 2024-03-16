d023cf39394f99cb9779b0da83add15a4917f461
lucas66764 <142950637+lucas66764@users.noreply.github.com>
2024-03-16 20:56:13 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+import { Modal } from './modal.js'
+import { AlertError} from "./alert-error.js"
+import { IMC, notNumber } from './utilis.js'
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+
+form.onsubmit = event => {
+  event.preventDefault()
+
+  const weight = inputWeight.value 
+  const height = inputHeight.value
+
+  const showAlertError = notNumber(weight) || notNumber(height)
+
+  if(showAlertError) {
+    AlertError.open()
+    return;
+  }
+
+  
+  
+
+  const result = IMC(weight, height)
+  const message= `Seu IMC é de ${result}`
+
+  Modal.message.innerText = message
+  Modal.open()
+
+}
+
+
